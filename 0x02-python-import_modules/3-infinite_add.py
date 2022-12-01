677eaa3231f11254bd8a0a599d546ac568763890
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-02 01:11:49 +0300

    Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from sys import argv
-
-    sum = 0
-    argc = len(argv)-1
-    if argc == 0:
-        print(0)
-
-    for i in range(argc):
-        i += 1
-        sum = sum + int(argv[i])
-
-    print(sum)
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
