2d5adc20ee29c856b03ddc4598bf9c0c27c1f371
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-02 00:49:12 +0300

    making dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,14 @@ if __name__ == "__main__":
     from sys import argv
     
     argc = len(argv) - 1
-    print(argc)
     if argc == 0:
         print("0 arguments.")
 
-    elif argc > 1:
-        print(f"{argc-1} arguments:")
+    elif argc == 1:
+        print("1 argument:")
 
     else:
-        print(f"{argc} argument:")
-    for i in range(argc-1):
+        print(f"{argc} arguments:")
+    for i in range(argc):
         i += 1
         print(f"{i}: {argv[i]}")
