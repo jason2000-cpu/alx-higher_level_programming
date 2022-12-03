3dc636731f20db0a3bb7c1e646e4dff4dbb73737
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-03 11:32:24 +0300

    remove letter in string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,10 @@
+#!/usr/bin/python3
+
 def no_c(my_string):
-    
+    str = list(my_string)
+    for i in range(0, len(str)):
+         if str[i] == 'c' or str[i] == 'C':
+             str[i] = ""
+         
+    return "".join(str)     
+      
