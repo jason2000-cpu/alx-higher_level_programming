19f73c3b6647472a30535050acc3b258d9c28910
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-04 17:29:47 +0300

    delete at index, alternative of POP() method

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
 
-    my_list_cpy = my_list.copy()
-    my_list_cpy.remove(my_list_cpy[idx])
-    return my_list_cpy
+    my_list.remove(my_list[idx])
+    return my_list
