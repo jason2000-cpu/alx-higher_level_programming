da4ff93c1128f7fc5d7b095375335c7f6c0b215b
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-04 14:57:50 +0300

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,9 +4,7 @@ def multiple_returns(sentence):
     info = ()
     if not sentence:
         info = info + (len(sentence), None)
-        print(info)
     else:
         info = info + (len(sentence), sentence[0])
-        print(info)
 
     return info
