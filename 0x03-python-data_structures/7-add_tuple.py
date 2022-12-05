0c6c535d3f2b672a98ee3a776594a2f996db0262
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-05 09:16:29 +0300

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,31 +1,7 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    
-    if type(tuple_a) is int:
-        tuple_a = (tuple_a, 0)
-
-    if type(tuple_b) is int:
-        tuple_b = (tuple_b, 0)
-    
-    if not tuple_a:
-        tuple_a = (0, 0)
-
-    if not tuple_b:
-        tuple_b = (0,0)
-
-    tupleList = []
-    for i in range(2):
-        tupleList.append(tuple_a[i] + tuple_b[i])
-
-    return tuple(tupleList)
-
-
-
-tuple_a = (1,)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, (1,))
-print(new_tuple)
-
-# print(add_tuple(tuple_a, (1, )))
-# print(add_tuple(tuple_a, ()))
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
