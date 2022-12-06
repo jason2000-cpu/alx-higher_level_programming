a9fc07bf4e7135f87f3ae43c9da1379c4ea24612
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-07 00:19:07 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list_cpy = my_list.copy()
-    for i in range(len(my_list_cpy)):
-        if my_list_cpy[i] == search:
-            my_list_cpy[i] = 89
+    #my_list_cpy = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = 89
 
-    return my_list_cpy
+    return my_list
