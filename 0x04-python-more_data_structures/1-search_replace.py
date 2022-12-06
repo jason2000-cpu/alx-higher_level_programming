0e0a784c6188486089027b664780cffc5cc7c8dd
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-07 00:21:18 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     my_list_cpy = my_list.copy()
     for i in range(len(my_list_cpy)):
         if my_list_cpy[i] == search:
-            my_list[i] = replace
+            my_list_cpy[i] = replace
 
     return my_list_cpy
