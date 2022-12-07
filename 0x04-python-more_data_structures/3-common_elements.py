a0aa0fa57aeab1e324e761f73f1f9dba6f7adb47
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-07 15:32:38 +0300

    retrn elements present in both lists

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,9 @@
 
 def common_elements(set_1, set_2):
     sorted_list = []
-    for i in range(len(set_1)):
-        for j in range(len(set_2)):
-            if set_1[i] == set_2[j]:
-                sorted_list.append(set_1[i])
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                sorted_list.append(j)
 
     return list(sorted_list)
