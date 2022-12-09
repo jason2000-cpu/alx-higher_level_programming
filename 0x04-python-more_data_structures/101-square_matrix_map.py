795bc3f4bba93f750bd70a2498426399d9592891
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-09 22:29:46 +0300

    return square matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    result = map(lambda x:  list(map(lambda y: y * y, x)), matrix_cpy)
-    return list(result)
+    return list(map(lambda x:  list(map(lambda y: y * y, x)), matrix_cpy))
