884bafb3bf71d92e8539c053ec1d4aa5241ecb46
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-09 10:45:44 +0300

    return weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,8 @@
 def weight_average(my_list=[]):
     summation = 0
     divident = 0
+    if not my_list:
+        return 0
     for i in range(len(my_list)):
         mul = 1
         divident += my_list[i][len(my_list[i])-1]
