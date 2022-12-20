f8873433cf5d2f6638a6f6702b23786036a513e2
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-20 12:37:10 +0300

    handling ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,10 +10,9 @@ def safe_print_division(a, b):
     """
 
     try:
-        quotient = a/b
-    except ZeroDivisionError:
+        quotient = a / b
+    except (ZeroDivisionError, TypeError):
         quotient = None
     finally:
         print("Inside result:{}".format(quotient))
-
     return (quotient)
