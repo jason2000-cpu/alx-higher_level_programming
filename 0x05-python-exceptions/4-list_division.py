c6e2b67b6c751dfdb4753609f0d509985c36b4ae
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-20 17:59:21 +0300

    safe list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,17 +14,18 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             quotient = my_list_1[i] / my_list_2[i]
-            div_Arr.append(quotient)
         except TypeError:
-            div_Arr.append(0)
+            quotient = 0
             i += 1
             print("wrong type")
         except IndexError:
-            div_Arr.append(0)
+            quotient = 0
             print("out of range")
         except ZeroDivisionError:
-            div_Arr.append(0)
+            quotient = 0
             i += 1
             print("division by 0")
+        finally:
+            div_Arr.append(quotient)
 
     return div_Arr
