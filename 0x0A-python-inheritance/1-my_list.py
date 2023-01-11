c609e4457e49b6bc87a293d1603e91eeb3ff9d57
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-01-11 16:46:56 +0300

    class inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,20 +3,9 @@
 
 
 class MyList(list):
-    """Initializing class MyList"""
+    """Initializing class MyList."""
     def __init__(self):
         pass
-    """A method to sort the list tuple. It returns a sorted list"""
-    def print_sorted(self, list):
-        """ Try catch block to try for a non int value"""
-        try:
-            for i in range(len(list)):
-                for j in range(len(list)):
-                    if list[i] < list[j]:
-                        temp = list[i]
-                        list[i] = list[j]
-                        list[j] = temp
-        except TypeError:
-            print("Not an int Tuple")
-
-        return list
+    """Print a list in sorted ascending order."""
+    def print_sorted(self):
+        return (sorted(self))
