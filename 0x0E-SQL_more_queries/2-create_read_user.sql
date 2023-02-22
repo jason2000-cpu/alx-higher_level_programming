481b9a82d340cf592581eea698c18d2b35f702cc
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 00:03:51 +0300

    task2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- This script;
+-- creates a database 'hbtn_0d_2
+-- creates user 'user_0d_2'
+-- 'user_0d_2 is given SELECT PRIVILEGE
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
