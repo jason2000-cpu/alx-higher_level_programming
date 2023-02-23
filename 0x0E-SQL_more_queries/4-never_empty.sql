1f5657807b932fa21bae2634255946fc18c9a4bf
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 10:12:16 +0300

    giving id column default value

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 --Scipt creates a table 'id_not_null'
 --id column of the table 'id_not_null' is given a default value of 1
-CREATE TABLE id_not_null(
+CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
