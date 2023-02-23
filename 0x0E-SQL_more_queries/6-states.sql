412b758125ed8ea91fa954ff5f8b397c520797da
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 11:56:29 +0300

    task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,7 +5,7 @@ USE `hbtn_0d_usa`;
 -- Script creates table 'states' in database 'hbtn_0d_usa' created
 -- id colun in table 'states is given 'AUTO', 'NOT NULL'  and 'PRIMARY KEY' constraints
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE AUTOINCREMENT NOT NULL,
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id)
 );
