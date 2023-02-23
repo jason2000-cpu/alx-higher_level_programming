f3ba31d04c7e13f233c88c3d986d0b1ce4440473
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 11:59:53 +0300

    task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,9 @@
--- Script creates database 'hbtn_0d_usa'
+
+
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---use data base creaeted
+-- use a database
 USE hbtn_0d_usa;
--- Script creates table 'states' in database 'hbtn_0d_usa' created
--- id colun in table 'states is given 'AUTO', 'NOT NULL'  and 'PRIMARY KEY' constraints
-CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
-);
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
