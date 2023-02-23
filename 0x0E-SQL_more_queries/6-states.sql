ad1f90ef9dae9e44c5ef860eb0210696e9561ad1
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 11:48:32 +0300

    task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,7 +3,4 @@
 -- id colun in table 'states is given 'AUTO', 'NOT NULL'  and 'PRIMARY KEY' constraints
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS states(
-    id INT AUTO NOT NULL,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS states(id INT AUTO NOT NULL,name VARCHAR(256));
