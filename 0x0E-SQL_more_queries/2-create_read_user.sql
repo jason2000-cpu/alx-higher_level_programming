3eb81cfd987c2c36ccf3a66ffce7384e74935169
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 08:40:43 +0300

    task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@
 -- 'user_0d_2 is given SELECT PRIVILEGE
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT  ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
