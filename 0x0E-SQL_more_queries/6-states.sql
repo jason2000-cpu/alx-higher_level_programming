3d11359a6d6ef2d356b310155d2b7cbd7a67c619
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 11:49:47 +0300

    task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Script creates database 'hbtn_0d_usa'
--- Script creates table 'states' in database 'hbtn_0d_usa' created
 -- id colun in table 'states is given 'AUTO', 'NOT NULL'  and 'PRIMARY KEY' constraints
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+--use data base creaeted
 USE `hbtn_0d_usa`;
+-- Script creates table 'states' in database 'hbtn_0d_usa' created
 CREATE TABLE IF NOT EXISTS states(id INT AUTO NOT NULL,name VARCHAR(256));
