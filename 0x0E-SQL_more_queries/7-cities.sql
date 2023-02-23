a8bd867936ea41f3555a69cf98b5c047e61e75ae
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 12:22:07 +0300

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
---Script creates a database 'hbtn_0d_usa'
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Script to use the database created
 USE hbtn_0d_usa;
