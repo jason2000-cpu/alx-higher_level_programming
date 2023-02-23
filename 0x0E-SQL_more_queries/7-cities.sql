74b28d40ec529ef1b362890044352b9a4e5a9753
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-02-23 12:14:13 +0300

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,4 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Script to use the database created
 USE hbtn_0d_usa;
 -- Script to create table 'cities' in the database 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    state_id INT NOT NULL FOREIGN KEY (states(id)),
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
-);
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL FOREIGN KEY (states(id)), name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
