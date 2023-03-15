7d404ff84a2ff581730f9d3cbb51fad064eb6fdd
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-15 16:26:51 +0300

    print the addition of two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  console.log(+a + +b)
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+function add (a, b) {
+  console.log(+a + +b);
 }
+
+add(firstInt, secondInt);
