395d3a1a868bc10298196d496ca819153af057fe
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-15 14:58:07 +0300

    convert stdin args to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+
+const number = parseInt(process.argv[2]);
 
 
-if (argv[2]) {
-  console.log(typeof(parseInt(argv[2])))
-} else {
+if (Number.isNaN(number)) {
   console.log('Not a number')
+} else {
+  console.log('My number: ' + number);
 }
