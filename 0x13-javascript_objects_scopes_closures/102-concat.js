b5a9bf675ffd57a172f9177c37b846e5e0256436
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-26 23:01:54 +0300

    commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,14 @@
-#!/usr/bin/node
 const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const file1Path = process.argv[2];
+const file2Path = process.argv[3];
+const outputPath = process.argv[4];
+
+const file1Contents = fs.readFileSync(file1Path, 'utf8');
+const file2Contents = fs.readFileSync(file2Path, 'utf-8');
+
+const outputFileContents = `${file1Contents} ${file2Contents}`;
+
+fs.writeFileSync(outputPath, outputFileContents);
+
+console.log(outputFileContents);
