945a23ec9f043068b58a831ab903ab552d3e50d9
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-28 12:28:19 +0300

    filter data from database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a 'name' 
+starting with 'N' (uppercase N) from the database
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[3], port=3306)
+    cur = db.cursor()
+    # cur.execute("SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ")
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
