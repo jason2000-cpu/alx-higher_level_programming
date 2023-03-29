e2e0baa7ab028a3c685a5d080d8993b13bc8f48a
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-29 12:05:37 +0300

    creating class instance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """
 Contains State class and Base, an instance of declarative_base()
 """
-
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,13 +10,11 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class states(Base):
-
     """
     Class with id and name attributes of each state
     Args:
         Base (class instance): this class iherits from the Base class
     """
-
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
