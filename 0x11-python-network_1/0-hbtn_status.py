d6b4e603c24964f2f87ec92da069ebeaf901e332
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-04-04 14:33:05 +0300

    script to fetch a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ import urllib.request
 A Python script that fetches 'https://alx-intranet.hbtn.io/'
 """
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/') as response:
-    html = response.read()
-    print(html)
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/') as response:
+        html = response.read()
+        print(html)
